217be6f94d2b57569ccd4e2429ac4539ce253d6f
CLickToREturns <43514034+Ryzen72700@users.noreply.github.com>
2019-08-31 21:39:10 +0900

    Update HI-FI_QuadDAC_main.py

--- HI-FI_QuadDAC_main.py
+++ HI-FI_QuadDAC_main.py
@@ -26,10 +26,11 @@ async def on_ready():
 # Commands : Normal
 @client.command(pass_context=True)
 async def help(ctx):
+    bid = client.user.id
     embed = discord.Embed(
-            title="HI-FI QuadDAC HELP",
-            description="★ 기본 접두사 : **h-**\n★ 현재 버전 : **1.0**\n\n==============================\n\n모든 명령어를 볼려면 **[여기](http://teamempty.dothome.co.kr)**를 클릭해주세요!",
-            color=0xffff00
+            title="[ 도움말 ] :: [ ** 봇 ID : " + str(bid) + "** ]",
+            description="★ 기본 접두사 : **h-**\n★ 현재 버전 : **BETA Service Mode**\n\n==============================\n\n모든 명령어를 볼려면 **[여기](http://teamempty.dothome.co.kr)**를 클릭해주세요!",
+            color=0x99ccff
         )
     await ctx.send(embed=embed)
 
@@ -38,7 +39,7 @@ async def 생활정보(ctx):
     embed = discord.Embed(
             title="생 활 정 보",
             description="**[전국날씨](https://search.naver.com/search.naver?ie=UTF-8&query=%EC%A0%84%EA%B5%AD%EB%82%A0%EC%94%A8&sm=chr_hty)**,**[준비중입니다]**",
-            color=0x00ffff
+            color=0x99ffff
         )
     await ctx.send(embed=embed)
 
@@ -46,18 +47,17 @@ async def 생활정보(ctx):
 async def info(ctx):
     embed = discord.Embed(
             title="Bot Running Information",
-            description="현재 돌아가고 있는곳 :\nㄴ Lenovo K3 Note(with MIUI)에서 구동 중\n\n프로그래밍 언어 :\nㄴ Python 3.6.2",
+            description="현재 돌아가고 있는곳 :\nㄴ 개발자 노트북에서 구동 중..\n\n프로그래밍 언어 :\nㄴ Python 3.7.0",
             color=0xffff00
         )
     await ctx.send(embed=embed)
 
 @client.command(pass_context=True)
-async def botinfo(ctx):
-    bid = client.user.id
+async def partner(ctx):
     embed = discord.Embed(
-            title="Bot Information",
-            description="**봇 ID** : " + str(bid) + "\n**봇 업타임** : 알수 없음\n**봇 소유자** : ClickToREturns(깃허브 참고)\n**업데이트 간격** : 그런거 몰라요, 깃허브 보세요",
-            color=0xffff00
+            title="**TEAM. EMPTY** Parner Server",
+            description="★ 파란색 글씨를 클릭시 접속됩니다.\n\n[대서 홍보방](https://discord.gg/yjq4mTv) - 자유 홍보서버(개발자 추천)\n**[ 새로운 파트너 모집중입니다 ]**",
+            color=0x99ff99
         )
     await ctx.send(embed=embed)
 
